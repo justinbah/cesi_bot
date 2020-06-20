84bbddb9d4bf05c6b084c0d8c3c6ce07982d3d39
justinbah <56537939+justinbah@users.noreply.github.com>
2020-06-20 19:11:37 +0200

    liste des commande
    j'ai ajouter la liste des commande

--- index.js
+++ index.js
@@ -3,7 +3,48 @@ const config = require('./config.json');
 const client = new Discord.Client();
 
 client.once('ready', () => {
-	console.log('Ready!');
+    console.log('Ready!');
 });
 
 client.login(config.token);
+
+
+client.on('message', message => {
+    var MessageMinuscule = message.content.toLowerCase()//afin de pas prendre en compte les majuscule ou minuscule 
+    if (user.id !== client.user.id && )//pour ignorer les msg du bot
+    {
+        if (contenuMessage === '!help') {
+
+        }
+
+
+        if (contenuMessage === '!présent' || contenuMessage === '!présente') {
+
+        }
+
+        if (contenuMessage === '!liste élève') {
+
+        }
+
+        if (contenuMessage.slice(0, 10) == '!présences') {//si message commence par !présences
+
+        }
+        if (contenuMessage.slice(0, 6) == '!cours') {//si message commence par !cours
+
+        }
+        if (contenuMessage.slice(0, 5) == '!link') {//si message commence par !link
+
+        }
+        if (contenuMessage.slice(0, 7) == '!delete') {//si message commence par !delete
+
+        }
+
+    }
+
+
+
+
+
+
+
+});
